3d7c1b8815d4ac5517e36c785be486b2a099fafe
Gustavo MS <gustavopoa@gmail.com>
2020-04-06 19:10:55 +0100

    clean code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify, request, render_template
+from flask import jsonify, request, render_template
 import requests
 import json
 
@@ -8,7 +8,6 @@ app.config["DEBUG"] = True
 
 
 def get_totals(data_cases):
-
     total = {
         'total_confirmed': sum(item['confirmed'] for item in data_cases['data']),
         'total_deaths': sum(item['deaths'] for item in data_cases['data']),
@@ -27,40 +26,9 @@ def home():
         params=params
     )
     result = json.loads(data.text)
-
     total = get_totals(result)
 
     return render_template('countries.html', countries=result, total=total)
-    # return json.loads(data.text)
-
-
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
 
 
 if __name__ == "main":
